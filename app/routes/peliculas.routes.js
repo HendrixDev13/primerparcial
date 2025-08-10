a08ca0515c5a4f6436847ebcc7d8fe70ae443775
HendrixDev13 <mendozahendrix9@gmail.com>
2025-08-09 18:03:53 -0600

    arreglos busqueda por nombre

--- app/routes/peliculas.routes.js
+++ app/routes/peliculas.routes.js
@@ -2,27 +2,27 @@ module.exports = app => {
     const peliculas = require("../controllers/peliculas.controllers.js");
     var router = require("express").Router();
 
-    // Create a new Pelicula
+   
     router.post("/create", peliculas.create);
 
-    // Retrieve all Peliculas
+   
     router.get("/", peliculas.findAll);
 
-    // Retrieve all Peliculas de drama (ejemplo)
+
     router.get("/drama", peliculas.findAllDrama);
 
-    // Retrieve a single Pelicula with id
+  
     router.get("/:id", peliculas.findOne);
 
-    // Update a Pelicula with id
+   
     router.put("/update/:id", peliculas.update);
 
-    // Delete a Pelicula with id
+  
     router.delete("/delete/:id", peliculas.delete);
 
-    // Delete all Peliculas
+  
     router.delete("/delete", peliculas.deleteAll);
 
-    // Endpoint base
+   
     app.use("/api/peliculas", router);
 };
